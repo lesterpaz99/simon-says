0a9ef3b4befb17233a0f36382bdb1a6f2cd55405
Obed Paz <lesterpaz99@gmail.com>
2021-02-08 11:37:09 -0600

    add level and a colors object to the Game class

--- index.js
+++ index.js
@@ -9,15 +9,23 @@ const btnStart = document.getElementById('btn-start');
 class Game {
   constructor() {
     this.initialize();
-    this.generateFrequency();
+    this.generateSequence();
   }
 
   initialize() {
     btnStart.classList.add("hide");
+    this.level = 1;
+    this.colors = {
+      green,
+      red,
+      yellow,
+      blue
+    }
   }
 
-  generateFrequency() {
+  generateSequence() {
       this.sequence = new Array(10).fill(0).map(() => Math.floor(Math.random() * 4));
+      /*La función map no funciona cuando los elementos no están definidos en un array y no tienen un valor. Es por ello que necesitan un valor aunque sea 0*/
   }
 }
 
