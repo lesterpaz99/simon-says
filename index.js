86c39368093eb408f97e1ab55a45bd50000fed1d
Obed Paz <lesterpaz99@gmail.com>
2021-02-07 22:13:40 -0600

    generate a sequence of numbers in an array

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const yellow = document.getElementById('yellow');
 const blue = document.getElementById('blue');
 const btnStart = document.getElementById('btn-start');
 
+//Ctrl + D para recorrer y editar mismas palabras en el codigo
+
 class Game {
   constructor() {
     this.initialize();
+    this.generateFrequency();
   }
 
   initialize() {
     btnStart.classList.add("hide");
+  }
+
+  generateFrequency() {
+      this.sequence = new Array(10).fill(0).map(() => Math.floor(Math.random() * 4));
   }
 }
 
